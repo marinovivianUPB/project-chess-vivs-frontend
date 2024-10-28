f76ff279f443a13beb600b5f98f301ddbd8d8bff
Khenya-dev <brenda.alvarado@assuresoft.com.bo>
2024-10-27 23:52:14 -0400

    chessboard

---
+++ src/chessboard/components/Board.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Chessboard } from 'react-chessboard';
+
+const Board = () => {
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+      <Chessboard />
+    </div>
+  );
+};
+
+export default Board;
