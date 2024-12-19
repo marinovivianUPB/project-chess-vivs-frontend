e30566cff95563f11a543d206641c94004647760
Vivian Marino <marino6733784@gmail.com>
2024-12-19 01:04:00 -0400

    correccion de backend.tf

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
 terraform {
  backend "s3" {
-   bucket         = "chess-mentor-vivs-frontend-block"
+   bucket         = "chess-mentor-frontend-vivs-block"
    key            = "global/s3/terraform.tfstate"
    region         = "us-east-1"
-   dynamodb_table = "chess-mentor-vivs-frontend-block"
+   dynamodb_table = "chess-mentor-frontend-vivs-block"
    encrypt        = true
  }
 }
